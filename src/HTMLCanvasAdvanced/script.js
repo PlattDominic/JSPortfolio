7566bc42dd74c15981a9993bab20e7a54cd9b2dc
PlattDominic <74417360+PlattDominic@users.noreply.github.com>
2023-02-02 14:03:54 -0500

    Began the deep dive of HTML canvas

---
+++ src/HTMLCanvasAdvanced/script.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    flowField.animate();
+}
+
+
+class FlowFieldEffect {
+    #ctx;
+    #width;
+    #height;
+
+    constructor(ctx, width, height) {
+        this.#ctx = ctx;
+        this.#ctx.strokeStyle = 'white';
+        this.#width = width;
+        this.#height = height;
+    }
+    #draw(x, y) {
+        const length = 300;
+        this.#ctx.beginPath();
+        this.#ctx.moveTo(x, y);
+        this.#ctx.lineTo(x + length, y + length);
+        this.#ctx.stroke();
+    }
+    animate() {
+        this.#draw(Math.random() * 100, 100);
+        console.log(3);
+        requestAnimationFrame(this.animate.bind(this));
+    }
+}

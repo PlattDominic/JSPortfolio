15025fd886abdb721c7a1065460b7ba8d1ff8b3c
PlattDominic <74417360+PlattDominic@users.noreply.github.com>
2023-02-06 13:51:18 -0500

    Finished the deep dive of canvas

--- src/HTMLCanvasAdvanced/script.js
+++ src/HTMLCanvasAdvanced/script.js
@@ -1,11 +1,33 @@
+let canvas;
+let ctx;
+let flowField;
+let flowFieldAnimation;
+
 window.onload = () => {
-    const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
+    canvas = document.getElementById('canvas1');
+    ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-    flowField.animate();
+    flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    flowField.animate(0);
 }
+
+window.addEventListener('resize', () => {
+    cancelAnimationFrame(flowFieldAnimation);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    flowField.animate(0);
+}); 
+
+const mouse = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+})
 
 
 class FlowFieldEffect {
@@ -15,20 +37,66 @@ class FlowFieldEffect {
 
     constructor(ctx, width, height) {
         this.#ctx = ctx;
-        this.#ctx.strokeStyle = 'white';
+        this.#ctx.lineWidth = 0.5;
         this.#width = width;
         this.#height = height;
+        this.lastTime = 0;
+        this.interval = 1000/60;
+        this.timer = 0;
+        this.cellSize = 15;
+        this.gradient;
+        this.#createGradient();
+        this.#ctx.strokeStyle = this.gradient;
+        this.radius = 0;
+        // The velocity of the radius ^
+        this.vr = 0.03;
     }
-    #draw(x, y) {
-        const length = 300;
+    #createGradient() {
+        this.gradient = this.#ctx.createLinearGradient(0, 0, this.#width, this.#height);
+        this.gradient.addColorStop("0.1", "#ff5c33");
+        this.gradient.addColorStop("0.2", "#ff66b3");
+        this.gradient.addColorStop("0.4", "#ccccff");
+        this.gradient.addColorStop("0.6", "#b3ffff");
+        this.gradient.addColorStop("0.8", "#80ff80");
+        this.gradient.addColorStop("0.9", "#ffff33");
+
+    }
+    #drawLine(angle, x, y) {
+        let positionX = x;
+        let positionY = y;
+        let dx = mouse.x - positionX;
+        let dy = mouse.y - positionY;
+        let distance = dx * dx + dy * dy;
+        if (distance > 600000) distance = 600000; 
+        else if (distance < 50000) distance = 50000;
+
+        let length = distance/10000;
         this.#ctx.beginPath();
         this.#ctx.moveTo(x, y);
-        this.#ctx.lineTo(x + length, y + length);
+        this.#ctx.lineTo(x + Math.cos(angle) * length, y + Math.sin(angle) * length);
         this.#ctx.stroke();
     }
-    animate() {
-        this.#draw(Math.random() * 100, 100);
-        console.log(3);
-        requestAnimationFrame(this.animate.bind(this));
+    animate(timeStamp) {
+        let deltaTime = timeStamp - this.lastTime;
+        this.lastTime = timeStamp;
+        if (this.timer > this.interval) {
+            this.#ctx.clearRect(0, 0, this.#width, this.#height);
+            this.radius += this.vr;
+            if (this.radius > 5 || this.radius < -5) this.vr *= -1;
+
+            for (let y = 0; y < this.#height; y += this.cellSize) {
+                for (let x = 0; x < this.#width; x += this.cellSize) {
+                    const angle = (Math.cos(mouse.x *  x * 0.00005) + Math.sin(mouse.y * y * 0.00005)) * this.radius;
+                    this.#drawLine(angle, x, y);
+                }
+            }
+            
+            this.timer = 0;
+        } else {
+            this.timer += deltaTime;
+        }
+
+        
+        flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
     }
 }

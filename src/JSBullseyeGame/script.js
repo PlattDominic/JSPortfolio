96b6b5e51932c10f6df1b867c9a926b69c716053
PlattDominic <74417360+PlattDominic@users.noreply.github.com>
2023-02-10 13:06:00 -0500

    Continued working on the bulleye game
    time stamp: 20:14

--- src/JSBullseyeGame/script.js
+++ src/JSBullseyeGame/script.js
@@ -4,9 +4,28 @@ window.addEventListener('load', () => {
     canvas.width = 1280;
     canvas.height = 720;
 
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
+
     class Player {
         constructor(game) {
             this.game = game;
+            this.collisionX = this.game.width * 0.5;
+            this.collisionY = this.game.height * 0.5;
+            this.collisionRadius = 30;
+        }
+
+        draw(context) {
+            context.beginPath();
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+            // the save function saves a snapchat of current canvas
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            // The restore function then restores the snapchat the was taken
+            context.restore();
+            context.stroke();
         }
     }
 
@@ -15,9 +34,29 @@ window.addEventListener('load', () => {
             this.canvas = canvas;
             this.width = this.canvas.width;
             this.height = this.canvas.height;
+
             this.player = new Player(this);
+            this.mouse = {
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
+            }
+
+            this.canvas.addEventListener('mousedown', e => {
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                console.log(this.mouse.x);
+                console.log(e.offsetX, e.offsetY);
+            })
+        }
+
+        render(context) {
+            this.player.draw(context);
         }
     }
+
+    const game = new Game(canvas);
+    game.render(ctx);
 
     function animate() {
 

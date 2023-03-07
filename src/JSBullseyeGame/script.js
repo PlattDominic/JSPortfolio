9d7d4e625d5e214d32739155bd99b1325630fd19
PlattDominic <74417360+PlattDominic@users.noreply.github.com>
2023-03-07 13:56:12 -0500

    Add Deltatime to bullseyes game
    Left off at timestamp 1:33:52

--- src/JSBullseyeGame/script.js
+++ src/JSBullseyeGame/script.js
@@ -16,7 +16,7 @@ window.addEventListener('load', () => {
             this.game = game;
             this.collisionX = this.game.width * 0.5;
             this.collisionY = this.game.height * 0.5;
-            this.collisionRadius = 40;
+            this.collisionRadius = 30;
             this.speedX = 0;
             this.speedY = 0;
             this.dx = 0;
@@ -24,7 +24,7 @@ window.addEventListener('load', () => {
             this.speedModifier = 5;
 
             this.spriteWidth = 255;
-            this.spriteHeight = 255;
+            this.spriteHeight = 256;
             this.width = this.spriteWidth;
             this.height = this.spriteHeight;
             this.spriteX;
@@ -38,32 +38,47 @@ window.addEventListener('load', () => {
             context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, 
                 this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, 
                 this.width, this.height); 
-            context.beginPath();
-            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
-            // the save function saves a snapchat of current canvas
-            context.save();
-            context.globalAlpha = 0.5;
-            context.fill();
-            // The restore function then restores the snapchat the was taken
-            context.restore();
-            context.stroke();
-            context.beginPath();
-            context.moveTo(this.collisionX, this.collisionY);
-            context.lineTo(this.game.mouse.x, this.game.mouse.y);
-            context.stroke();
+
+            if (this.game.debug) {
+                context.beginPath();
+                context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+                // the save function saves a snapchat of current canvas
+                context.save();
+                context.globalAlpha = 0.5;
+                context.fill();
+                // The restore function then restores the snapchat the was taken
+                context.restore();
+                context.stroke();
+                context.beginPath();
+                context.moveTo(this.collisionX, this.collisionY);
+                context.lineTo(this.game.mouse.x, this.game.mouse.y);
+                context.stroke(); 
+            }
         }
         update() {
 
             this.dx = this.game.mouse.x - this.collisionX;
             this.dy = this.game.mouse.y - this.collisionY;
             const angle = Math.atan2(this.dy, this.dx);
-            if (angle < -1.17) this.frameY = 0;
-            else if (angle < -0.39) this.frameY = 1;
-            else if (angle < 0.39) this.frameY = 2;
-            else if (angle < 1.17) this.frameY = 3;
-            else if (angle < -1.17) this.frameY = 3;
-            else if (angle < 1.96) this.frameY = 3;
-            console.log(angle);
+
+            // if (angle < -2.74 || angle > 2.74) this.frameY = 6;
+            // else if (angle < -1.96) this.frameY = 7;
+            // else if (angle < -1.17) this.frameY = 0;
+            // else if (angle < -0.39) this.frameY = 1;
+            // else if (angle < 0.39) this.frameY = 2;
+            // else if (angle < 1.17) this.frameY = 3;
+            // else if (angle < 1.96) this.frameY = 4;
+            // else if (angle < 2.74) this.frameY = 5;
+            
+            if (angle > -1.96 && angle <-1.18) this.frameY = 0;
+            else if (angle > -1.18 && angle <-0.39) this.frameY = 1;
+            else if (angle > -0.39 && angle < 0.39) this.frameY = 2;
+            else if (angle >  0.39 && angle < 1.18) this.frameY = 3;
+            else if (angle >  1.18 && angle < 1.96) this.frameY = 4;
+            else if (angle >  1.96 && angle < 2.74) this.frameY = 5;
+            else if (angle >  2.74 || angle <-2.74) this.frameY = 6;
+            else if (angle > -2.74 && angle <-1.96) this.frameY = 7;
+            
             
             const distance = Math.hypot(this.dy, this.dx);
             
@@ -80,6 +95,19 @@ window.addEventListener('load', () => {
 
             this.spriteX = this.collisionX - this.width * 0.5;
             this.spriteY = this.collisionY - this.height * 0.5 - 100;
+
+            // horizontal boundaries
+            if (this.collisionX < this.collisionRadius) {
+                this.collisionX = this.collisionRadius;
+            } else if (this.collisionX > this.game.width - this.collisionRadius) {
+                this.collisionX = this.game.width - this.collisionRadius;
+            }
+            // vertical boundaries - remember vertical coordinates in canvas go down in value as you go up canvas
+            if (this.collisionY < this.game.topMargin + this.collisionRadius) {
+                this.collisionY = this.game.topMargin + this.collisionRadius;
+            } else if (this.collisionY > this.game.height - this.collisionRadius) {
+                this.collisionY = this.game.height - this.collisionY;
+            }
 
 
             // collisions with obstacles 
@@ -111,7 +139,7 @@ window.addEventListener('load', () => {
             this.game = game;
             this.collisionX = Math.random() * this.game.width;
             this.collisionY = Math.random() * this.game.height;
-            this.collisionRadius = 60;
+            this.collisionRadius = 40;
             this.image = document.getElementById('obstacles');
 
             this.spriteWidth = 250;
@@ -129,12 +157,15 @@ window.addEventListener('load', () => {
             context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, 
                 this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
             context.beginPath();
-            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
-            context.save();
-            context.globalAlpha = 0.5;
-            context.fill();
-            context.restore();
-            context.stroke();
+        
+            if (this.game.debug) {
+                context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+                context.save();
+                context.globalAlpha = 0.5;
+                context.fill();
+                context.restore();
+                context.stroke();
+            }
         }
     }
 
@@ -150,6 +181,12 @@ window.addEventListener('load', () => {
             this.obstacles = [];
             this.numberOfObstacles = 10;
 
+            this.debug = true;
+
+            this.fps = 70;
+            this.timer = 0;
+            this.interval = 1000/this.fps;
+
             this.mouse = {
                 x: this.width * 0.5,
                 y: this.height * 0.5,
@@ -160,18 +197,21 @@ window.addEventListener('load', () => {
                 this.mouse.x = e.offsetX;
                 this.mouse.y = e.offsetY;
                 this.mouse.pressed = true;
-            })
+            });
             this.canvas.addEventListener('mouseup', e => {
                 this.mouse.x = e.offsetX;
                 this.mouse.y = e.offsetY;
                 this.mouse.pressed = false;
-            })
+            });
             this.canvas.addEventListener('mousemove', e => {
                 if (this.mouse.pressed) {
                     this.mouse.x = e.offsetX;
                     this.mouse.y = e.offsetY;
                 }
-            })
+            });
+            window.addEventListener('keydown', e => {
+                if (e.key === 'd') this.debug = !this.debug;
+            });
         }
 
         checkCollision(a, b) {
@@ -201,7 +241,7 @@ window.addEventListener('load', () => {
                     }
 
                 });
-                const margin = testObstacle.collisionRadius * 2;
+                const margin = testObstacle.collisionRadius * 3;
                 if (!overlap && testObstacle.spriteX > 0 
                     && testObstacle.spriteX < this.width - testObstacle.width
                     && testObstacle.collisionY > this.topMargin + margin
@@ -213,20 +253,30 @@ window.addEventListener('load', () => {
             }
         }
 
-        render(context) {
-            this.player.draw(context);
-            this.player.update();
-            this.obstacles.forEach(obstacle => obstacle.draw(context));
+        render(context, deltaTime) {
+            if (this.timer > this.interval) {
+                // animate next frame
+                console.log(deltaTime)
+                ctx.clearRect(0, 0, this.width, this.height);
+                this.player.draw(context);
+                this.player.update();
+                this.obstacles.forEach(obstacle => obstacle.draw(context));
+
+                this.timer = 0;
+            }
+            this.timer += deltaTime;
         }
     }
 
     const game = new Game(canvas);
     game.init();
 
-    function animate() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.render(ctx);
+    let lastTime = 0;
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        game.render(ctx, deltaTime);
         requestAnimationFrame(animate);
     }
-    animate();
+    animate(0);
 });
